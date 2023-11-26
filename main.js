ea7805cab970a4fea718f5545dd739bb384710c2
Manuel Frezzato <manuel.frezzato@gmail.com>
2023-11-26 16:54:02 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+/*
+# Esercizio 1 - Posts
+Studiati come funzionano le API di https://jsonplaceholder.typicode.com/.
+Crea una pagina HTML in cui, dopo aver richiesto dei post all’API, vengono create delle card nel documento: per ogni card mostra titolo e contenuto.
+
+# Esercizio 2 - Barzellette
+Studiati come funzionano le API di https://v2.jokeapi.dev/.
+Crea una pagina HTML in cui l’utente può decidere quante barzellette visualizzare. Dopo che l’utente ha deciso e dato conferma d’invio (con un bottone), interroga l’API e crea una lista di barzellette che contiene tante barzellette (a tema Programming) quante ne ha richieste l’utente.
+
+# Esercizio 3 - Ricerca paesi per lingua
+Studiati come funzionano le API di https://restcountries.com/.
+Crea una pagina HTML in cui l’utente può inserire del testo ed effettuare una ricerca di paesi a partire dalla lingua. Dopo che l’utente ha scritto del testo e dato conferma d’invio (con un bottone), interroga l’API e crea una lista di cards, di cui ciascuna card è un risultato della ricerca. Ogni card rappresenta un paese cercato a partire dalla stringa inserita dall’utente (una lingua parlata nel paese).
+*/
