f8168d353eb26420f26e75daab5ced86ea50370f
Manuel Frezzato <manuel.frezzato@gmail.com>
2023-11-27 21:52:03 +0100

    feat | Esercizio due non riuscito

--- main.js
+++ main.js
@@ -2,7 +2,8 @@
 # Esercizio 1 - Posts
 Studiati come funzionano le API di https://jsonplaceholder.typicode.com/.
 Crea una pagina HTML in cui, dopo aver richiesto dei post all’API, vengono create delle card nel documento: per ogni card mostra titolo e contenuto.
-*/
+
+
 window.addEventListener('load', async (event) => {
     const posts = await fetchPosts();
     createNodes(posts);
@@ -16,7 +17,7 @@ async function fetchPosts() {
 
 function createNodes(posts) {
     const cardList = document.getElementById('list');
-
+    
     posts.forEach(post => {
         const div = document.createElement('div');
         const p1 = document.createElement('p');
@@ -29,6 +30,7 @@ function createNodes(posts) {
         cardList.appendChild(div);
     });
 }
+*/
 
 /*
 
@@ -37,7 +39,24 @@ function createNodes(posts) {
 # Esercizio 2 - Barzellette
 Studiati come funzionano le API di https://v2.jokeapi.dev/.
 Crea una pagina HTML in cui l’utente può decidere quante barzellette visualizzare. Dopo che l’utente ha deciso e dato conferma d’invio (con un bottone), interroga l’API e crea una lista di barzellette che contiene tante barzellette (a tema Programming) quante ne ha richieste l’utente.
+*/
 
+window.addEventListener('load', () => {
+    const button = document.getElementById('btn')
+    const input = document.getElementById('inputUtente')
+    button.addEventListener('click', async () => {
+        const response = await fetch(`https://v2.jokeapi.dev/joke/Any?type=single&amount=${input.value}`);
+        const joke = await response.json();
+        const div = document.createElement('div')
+        const p = document.createElement('p')
+        p.textContent = joke.jokes
+        div.appendChild(p)
+    })
+})
+
+// non riuscito
+
+/*
 # Esercizio 3 - Ricerca paesi per lingua
 Studiati come funzionano le API di https://restcountries.com/.
 Crea una pagina HTML in cui l’utente può inserire del testo ed effettuare una ricerca di paesi a partire dalla lingua. Dopo che l’utente ha scritto del testo e dato conferma d’invio (con un bottone), interroga l’API e crea una lista di cards, di cui ciascuna card è un risultato della ricerca. Ogni card rappresenta un paese cercato a partire dalla stringa inserita dall’utente (una lingua parlata nel paese).
